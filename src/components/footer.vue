794ccb5ec4e09b1f2d903d23c77ca766a6952992
SSS1267_ <sams3s1267@gmail.com>
2024-11-08 19:10:09 +0900

    Update footer.vue

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,7 +2,7 @@
     <div class="container flex mt-[7.5rem] text-gray-400 bg-gray-900 border-gray-700 max-w-screen-xl flex-wrap items-center justify-between mx-auto p-4">
         <div class="mx-auto md:mx-0 text-center md:text-start text-sm md:text-base">
             <a class="" href="/">
-                <img src="https://cdn.lunaiz.com/kghs/khbs.svg" class="mx-auto md:mx-0 w-[7.5rem]" alt="lunaiz logo" /></a>
+                <object data="https://cdn.lunaiz.com/kghs/khbs.svg" class="mx-auto md:mx-0 w-[7.5rem]" alt="lunaiz logo" /></a>
                 <div class="flex flex-wrap mx-auto text-[1.2rem] justify-center md:justify-start mt-4">
                 <div class="mr-8">
                     <a class="" href="https://www.instagram.com/khbs_kyunggi/"><i
@@ -19,7 +19,7 @@
         </div>
         <div class="mx-auto md:mx-0 text-center hidden md:block md:text-right text-sm md:text-base">
             <a class="" href="/">
-                <img src="https://cdn.lunaiz.com/kghs/kghs.svg" class="mx-auto md:mx-0 w-[12rem]" alt="lunaiz logo" /></a>
+                <object data="https://cdn.lunaiz.com/kghs/kghs.svg" class="mx-auto md:mx-0 w-[12rem]" alt="lunaiz logo" /></a>
             <p class="mt-2">서울특별시교육청 경기고등학교</p>
             <p class="mt-2">서울특별시 강남구 영동대로 643</p>
             <p class="mt-2">Ⓒ 2024 lunaiz, all rights reserved.</p>
