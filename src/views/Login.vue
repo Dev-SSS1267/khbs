1509f143af4dc2f2f2583041e714e75963ca6143
Dev-SSS1267 <sams3s1267@gmail.com>
2024-11-09 15:39:54 +0900

    Update Login.vue to use Korean language and update input placeholders

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,33 +1,35 @@
 <template>
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-      <div class="bg-white p-8 rounded shadow-md w-full max-w-sm">
-        <h2 class="text-2xl font-bold mb-4 text-center">Login</h2>
+    <div class="flex items-center justify-center min-h-[50vh] ">
+      <div class="p-8 rounded shadow-md w-full max-w-sm">
+        <h2 class="text-2xl font-bold mb-4 text-center">관리자 로그인</h2>
         <form @submit.prevent="handleLogin">
           <div class="mb-4">
-            <label class="block text-gray-700 mb-2" for="username">Username</label>
+            <label class="block text-gray-100 mb-2" for="username">아이디</label>
             <input
               v-model="username"
               id="username"
               type="text"
               required
-              class="w-full p-2 border border-gray-300 rounded"
+              placeholder="아이디를 입력하세요"
+              class="w-full p-2 border border-gray-600 rounded bg-gray-700"
             />
           </div>
           <div class="mb-6">
-            <label class="block text-gray-700 mb-2" for="password">Password</label>
+            <label class="block text-gray-100 mb-2" for="password">비밀번호</label>
             <input
               v-model="password"
               id="password"
               type="password"
               required
-              class="w-full p-2 border border-gray-300 rounded"
+                placeholder="비밀번호를 입력하세요"
+              class="w-full p-2 border border-gray-600 rounded bg-gray-700"
             />
           </div>
           <button
             type="submit"
             class="w-full bg-blue-500 text-white p-2 rounded font-semibold hover:bg-blue-600"
           >
-            Login
+            로그인
           </button>
           <p v-if="errorMessage" class="text-red-500 mt-4">{{ errorMessage }}</p>
         </form>
