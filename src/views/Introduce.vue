46e196c98b9c28f19d16e32ce4363ce897b7f81e
Dev-SSS1267 <sams3s1267@gmail.com>
2024-11-10 19:09:56 +0900

    Refactor Introduce.vue component to update content and add broadcast channels

--- src/views/Introduce.vue
+++ src/views/Introduce.vue
@@ -3,7 +3,8 @@
         <template #head-txt>소개</template>
     </Hero>
     <div class="max-w-screen-lg mx-auto px-4">
-        <p class="my-4 text-xl font-semibold">임원</p>
+        
+        <p class="mb-4 mt-10 text-xl font-semibold">임원</p>
         <!-- 구분선 -->
         <hr class="h-px border-0 mb-10 bg-gray-700">
         <!-- 카드 그리드 -->
@@ -17,7 +18,9 @@
                 </div>
             </div>
         </div>
-        <hr class="h-px border-0 my-10 bg-gray-700">
+        <p class="mb-4 mt-10 text-xl font-semibold">역대 임원</p>
+        <!-- 구분선 -->
+        <hr class="h-px border-0 mb-10 bg-gray-700">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-7">
             <!-- 오른쪽 열 -->
             <div v-for="(member, index) in rightColumn" :key="index"
@@ -25,6 +28,31 @@
                 <div>
                     <h3 class="text-xl font-semibold">{{ member.name }}</h3>
                     <p class="text-gray-400">{{ member.position }}</p>
+                </div>
+            </div>
+        </div>
+        <p class="mb-4 mt-10 text-xl font-semibold">방송 채널</p>
+        <!-- 구분선 -->
+        <hr class="h-px border-0 mb-10 bg-gray-700">
+        <!-- 카드 그리드 -->
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <!-- 왼쪽 열 -->
+            <div
+                class="bg-gray-800 shadow-md rounded-lg p-6 flex items-center space-x-4">
+                <div>
+                    <h3 class="text-xl font-semibold">CH 4-1</h3>
+                </div>
+            </div>
+            <div
+                class="bg-gray-800 shadow-md rounded-lg p-6 flex items-center space-x-4">
+                <div>
+                    <h3 class="text-xl font-semibold">CH 10-1</h3>
+                </div>
+            </div>
+            <div
+                class="bg-gray-800 shadow-md rounded-lg p-6 flex items-center space-x-4">
+                <div>
+                    <h3 class="text-xl font-semibold">CH 14-1</h3>
                 </div>
             </div>
         </div>
